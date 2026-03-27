53f3e7bd09792787dc8ec3bc194fa5bf96b96028
sadiejudd <160371066+sadiejudd@users.noreply.github.com>
2026-03-27 11:38:35 -0600

    final commit

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from .views import UploadView, HomeView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("upload/",UploadView.as_view(), name = "upload"),
-    path("", HomeView.as_view())
+    path("", HomeView.as_view(), name = "home")
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- gallery/views.py
+++ gallery/views.py
@@ -2,14 +2,15 @@ from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from django.views.generic import ListView
 from .models import Image
+from django.urls import reverse_lazy
 
 # Create your views here.
 
 class UploadView(CreateView):
     model = Image
-    fields = "__all__"
+    fields = ["image"]
     template_name = "gallery/upload.html"
-    success_url = "/home"
+    success_url = reverse_lazy("home")
 
 
 class HomeView(ListView):
