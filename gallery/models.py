8debd90b384366d868b07197eba05dcdd6529a00
sadiejudd <sadiejudd@mail.weber.edu>
2026-03-31 15:53:23 -0600

    added filtering for images

--- gallery/models.py
+++ gallery/models.py
@@ -3,6 +3,14 @@ from django.db import models
 
 # Create your models here.
 
+
+class Tag(models.Model):
+    tag = models.CharField(max_length=100, name = "tag")
+
+    def __str__(self):
+        return self.tag
+
 class Image(models.Model):
     image = models.ImageField()
     session = models.CharField(max_length=100)
+    tags = models.ManyToManyField(Tag)
--- gallery/views.py
+++ gallery/views.py
@@ -1,26 +1,65 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from django.views.generic import ListView
-from .models import Image
+from .models import Image, Tag
 from django.urls import reverse_lazy
+from django import forms
 
 # Create your views here.
 
+class ImageForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        fields = ["image", "tags"]
+        widgets = {
+            "tags": forms.CheckboxSelectMultiple()
+        }
+
+
+
+
 class UploadView(CreateView):
     model = Image
-    fields = ["image"]
+    form_class = ImageForm
     template_name = "gallery/upload.html"
     success_url = reverse_lazy("home")
 
-    def from_valid(self, form):
+  
+
+    def form_valid(self, form):
         if not self.request.session.session_key:
             self.request.session.create()
 
-        form.instance.session_id = self.request.session.session_key
+        form.instance.session= self.request.session.session_key
         return super().form_valid(form)
 
 
 class HomeView(ListView):
-    model = Image 
+    model = Image
     template_name = "gallery/home.html"
     context_object_name = "images"
+
+    def get_queryset(self):
+        if not self.request.session.session_key:
+            self.request.session.create()
+
+
+        query_set = super().get_queryset()
+        query_set = query_set.filter(session = self.request.session.session_key)
+
+        selected_tags = self.request.GET.getlist("tags")
+
+        if selected_tags:
+            query_set = query_set.filter(tags__id__in=selected_tags).distinct()
+        
+        return query_set
+
+    
+    
+    
+    def get_context_data(self, **kwargs):
+
+        context= super().get_context_data(**kwargs)
+        context["tags"] = Tag.objects.all()
+        return context
+    
