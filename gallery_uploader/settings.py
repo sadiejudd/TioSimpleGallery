200dba4e84b29cfa2f3dea7dbc9e956c676ef184
sadiejudd <sadiejudd@mail.weber.edu>
2026-04-16 17:53:53 -0600

    Added data to migration

--- gallery_uploader/settings.py
+++ gallery_uploader/settings.py
@@ -77,11 +77,20 @@ WSGI_APPLICATION = 'gallery_uploader.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv("DATABASE_URL")
-    )
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+
+if DATABASE_URL :
+    DATABASES = {
+        "default": dj_database_url.parse(DATABASE_URL)
 }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
